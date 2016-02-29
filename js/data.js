40b1dfae949fe8d2607e62bafe8bba8e69ada739
hui shen <hui shen>
2016-02-29 20:25:36 +0800

    IE

--- js/data.js
+++ js/data.js
@@ -20,7 +20,6 @@ var intr = new Vue({
 			}
 		});
 	},
-
 	methods:{
 		refresh: function(event) {
 			var that = this;
@@ -28,12 +27,13 @@ var intr = new Vue({
 			$.ajax({
 				url: 'http://120.27.137.151/api/infos-list?pageNum='+that.num,
 				success: function(data) {
-					that.infors = that.infors.concat(data.data.infors);	
+						// alert('213');
+					 for(var i=0;i<data.data.infors.length;i++){
+					 	that.infors.push(data.data.infors[i])
+					 }	
 				} 				
 			});
 		},
-
-
 		changeWidth:function() {
 			intr.$watch('infors',function(){
 						$(".block").each(function() {
@@ -52,41 +52,29 @@ var intr = new Vue({
 });
 intr.changeWidth();
 
-
-
-
-
 /*资料下载下载，*/
 var download = new Vue({
-	el: "#download",
+	el: "#down",
 	data: {
 		downloads: [
-			// {
-				// pushDay: "23",
-				// pushMouth: "12",
-				// resourceTitle: "我的前半生"
-			// }
+
 		]
 	},
-	methods:{
-		show:function(event){
+	created:function(){
 		var that = this;
 		$.ajax({
-		url: 'http://120.27.137.151/api/download/',
-		success: function(data) {
-					// console.log(data);
-					var downloadList=new Array();
-					for(var i=0;i<5;i++){
-							downloadList.push(data.data.downloads[i]);
-							}
-							that.downloads = downloadList;				
-					},
-				});	
-			}
-
+			url: 'http://120.27.137.151/api/download/',
+			success: function(data) {
+						var def=new Array();
+						for(var d=0;d<data.data.downloads.length;d++){
+						 	def.push(data.data.downloads[d])
+						 }					
+						that.downloads=def;
+						// alert(def);
+					}
+		});	
 	}
 });
-download.show();
 
 /*校园人物*/
 var person = new Vue({
@@ -101,13 +89,13 @@ var person = new Vue({
 		$.ajax({
 		url: 'http://120.27.137.151/api/person-list/',
 		success: function(data) {
-					// console.log(data);
-					var personNum=new Array();
-					for(var i=0;i<4;i++){
-							personNum.push(data.data.persons[i]);
-							}
-							that.persons = personNum;				
-					},
+					// var personNum=new Array();
+					// for(var i=0;i<4;i++){
+					// 		personNum.push(data.data.persons[i]);
+					// 		}
+					// 		that.persons = personNum;	
+					that.persons = that.persons.concat(data.data.persons);			
+					}
 				});	
 			},
 
@@ -123,39 +111,25 @@ person.show();person.change();
 var layout = new Vue({
 	el: "#layout",
 	data: {
-		// detail:[
-
-		// ]
 	},
 	methods:{
 		new:function(){
 		var that=this;
 		var Url=window.location.href;
-
 		var idNum=Url.split("?")[1]
-
 		var apiUrl="http://120.27.137.151/api/detail/?"+idNum;
-
-		
-
 		$.ajax({
 			url:apiUrl,	
 			success:function(data){
-					console.log(data.data);
-					// console.log(data.data);
 					var pageList=new Array();
 					for(var i=0;i<3;i++){
 						pageList.push(data.data);
 					}
-					// that.data=pageList;
 					layout.$data=pageList[0];
-
-
 			}
 		});
 		}
 	}
-	
 });
 layout.new();
 
