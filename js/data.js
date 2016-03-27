9b27ae374a9270db8842b7a023bf05a7c70eb3f7
hui shen <hui shen>
2016-03-27 15:54:25 +0800

    3-27

--- js/data.js
+++ js/data.js
@@ -1,200 +1,162 @@
 var intr = new Vue({
-	el: "#intr",
-	data: {
-		num: 1,
-		infors: [
+    el: "#intr",
 
-		],
-		isA: true,
-  		isB: true
-		
-	},
-	created: function() {
-		var that = this;
-		$.ajax({
-			url: 'http://120.27.137.151/api/infos-list/',
-			success: function(data) {
-				var abc=new Array();
-				for(i=0;i<7;i++){
-					abc.push(data.data.infors[i]);
-				}
-				that.infors = abc;			
-			}
-		});
-	},
-	methods:{
-		refresh: function(event) {
-			var that = this;
-			that.num=that.num+1;
-			$.ajax({
-				url: 'http://120.27.137.151/api/infos-list?pageNum='+that.num,
-				success: function(data) {
-						// alert('213');
-					 for(var i=0;i<data.data.infors.length;i++){
-					 	that.infors.push(data.data.infors[i])
-					 	}	
-					}
-					// function cssAn(){
-					// 	intr.$watch(,function)
-					// }				
-			});
-		},
-		changeWidth:function() {
-			intr.$watch('infors',function(){
-						$(".block").each(function() {
-							var firstChildTg = $(this).context.firstElementChild.tagName;
-							var firstChild = $(this)[0].firstElementChild;
-							var width = $(this)[0].firstElementChild.style.width;
-							if (firstChildTg == "IMG") {} else {
-								$(this)[0].firstElementChild.style.width = "92%";
-								$(this)[0].firstElementChild.lastElementChild.style.width = "48.5%";
-							}
-						});
+    data: {
+        num: 1,
+        infors: [
 
-					});
-				}
-	}
+        ],
+        isA: true,
+        isB: true
+
+    },
+    
+    created: function() {
+        var that = this;
+        // console.log('created')
+        jQuery.support.cors = true;
+        $.ajax({
+            url: 'http://120.27.137.151:8585/api/infos-list/',
+            type: 'GET',
+            dataType: "json",
+            crossDomain: true,
+            contentType: 'application/json; charset=utf-8',
+            success: function(data) {
+                // console.log('success')
+                that.infors = data.data.infors;
+            },
+            error: function(err) {
+                console.log(err)
+            }
+        });
+    },
+    methods: {
+        refresh: function(event) {
+            // console.log('click')
+            var that = this;
+            that.num = that.num + 1;
+            $.ajax({
+                url: 'http://120.27.137.151:8585/api/infos-list?pageNum=' + that.num,
+                success: function(data) {
+                    // console.log(data);
+                    that.infors = that.infors.concat(data.data.infors)
+
+                }
+
+            });
+        },
+        changeWidth: function() {
+            intr.$watch('infors', function() {
+                $(".block").each(function() {
+                    var firstChildTg = $(this).context.firstElementChild.tagName;
+                    var firstChild = $(this)[0].firstElementChild;
+                    var width = $(this)[0].firstElementChild.style.width;
+                    if (firstChildTg == "IMG") {} else {
+                        $(this)[0].firstElementChild.style.width = "92%";
+                        $(this)[0].firstElementChild.lastElementChild.style.width = "46.5%";
+                    }
+                });
+
+            });
+        }
+    }
 });
 intr.changeWidth();
 
-/*资料下载下载，*/
-// var download = new Vue({
-// 	el: "#down",
-// 	data: {
-// 		downloads: [
 
-// 		]
-// 	},
-// 	created:function(){
-// 		var that = this;
-// 		$.ajax({
-// 			url: 'http://120.27.137.151/api/download/',
-// 			success: function(data) {
-// 						var def=new Array();
-// 						for(var d=0;d<data.data.downloads.length;d++){
-// 						 	def.push(data.data.downloads[d])
-// 						 }					
-// 						that.downloads=def;
-// 						// alert(def);
-// 					}
-// 		});	
-// 	}
-// });
 
 var download = new Vue({
-	el: "#down",
-	data: {
-		downloads: [
-		]
-	},
-	methods:{
-		show:function(event){
-		var that = this;
-		$.ajax({
-		url: 'http://120.27.137.151/api/download/',
-		success: function(data) {
-					// console.log(data);
-					var downloadList=new Array();
-					for(var i=0;i<data.data.downloads.length;i++){
-							downloadList.push(data.data.downloads[i]);
-							}
-							that.downloads = downloadList;				
-					}
-				});	
-			}
+    el: "#down",
+    data: {
+        downloads: []
+    },
+    methods: {
+        show: function(event) {
+            var that = this;
+            jQuery.support.cors = true;
+            $.ajax({
+                url: 'http://120.27.137.151:8585/api/download/',
+                success: function(data) {
+                    var downloadList = new Array();
+                    for (var i = 0; i < data.data.downloads.length; i++) {
+                        downloadList.push(data.data.downloads[i]);
+                    }
+                    that.downloads = downloadList;
+                }
+            });
+        }
 
-	}
+    }
 });
 download.show();
 
-/*校园人物*/
-// var person = new Vue({
-// 	el: "#person",
-// 	data: {
-// 		persons: [
-// 			]
-// 		},
-// 	methods:{
-// 		show:function(event){
-// 		var that = this;
-// 		$.ajax({
-// 		url: 'http://120.27.137.151/api/person-list/',
-// 		success: function(data) {	
-// 					that.persons = that.persons.concat(data.data.persons);			
-// 					}
-// 				});	
-// 			},
 
-// 		change:function() {
-// 			person.$watch('persons',function(){
-// 						$('#person').children()[0].style.left='0px';
-// 					});
-// 				}
-// 		}
-// });
-// person.show();person.change();
 
 var person = new Vue({
-	el: "#person",
-	data: {
-		persons: [
-			]
-		},
-	methods:{
-		show:function(event){
-		var that = this;
-		$.ajax({
-		url: 'http://120.27.137.151/api/person-list/',
-		success: function(data) {
-					// console.log(data);
-					var personNum=new Array();
-					for(var i=0;i<4;i++){
-							personNum.push(data.data.persons[i]);
-							}
-							that.persons = personNum;				
-					}
-				});	
-			},
+    el: "#person",
+    data: {
+        persons: []
+    },
+    methods: {
+        show: function(event) {
+            var that = this;
+            $.ajax({
+                url: 'http://120.27.137.151:8585/api/person-list/',
+                type: 'GET',
+                dataType: "json",
+                crossDomain: true,
+                contentType: 'application/json; charset=utf-8',
+                success: function(data) {
 
-		change:function() {
+                    var personNum = new Array();
+                    for (var i = 0; i < 4; i++) {
+                        personNum.push(data.data.persons[i]);
+                    }
+                    that.persons = personNum;
+                }
+            });
+        },
 
-			person.$watch('persons',function(){
-						$('#person').children()[0].style.left='0px';
-						// alert("111");
-					});
-				}
-		}
+        change: function() {
+            person.$watch('persons', function() {
+                $('#person').children()[0].style.left = '0px';
+
+            });
+        }
+    }
 });
-person.show(); person.change();
+person.show();
+person.change();
 
 var layout = new Vue({
-	el: "#layout",
-	data: {
-	},
-	methods:{
-		new:function(){
-		var that=this;
-		var Url=window.location.href;
-		var idNum=Url.split("?")[1]
-		var apiUrl="http://120.27.137.151/api/detail/?"+idNum;
-		$.ajax({
-			url:apiUrl,	
-			success:function(data){
-					var pageList=new Array();
-					for(var i=0;i<3;i++){
-						pageList.push(data.data);
-					}
-					layout.$data=pageList[0];
-			}
-		});
-		}
-	}
+    el: "#layout",
+    data: {},
+    methods: {
+        new: function() {
+            var that = this;
+            jQuery.support.cors = true;
+            var Url = window.location.href;
+            var idNum = Url.split("?")[1]
+            var apiUrl = "http://120.27.137.151:8585/api/detail/?" + idNum;
+            $.ajax({
+                url: apiUrl,
+                type: 'GET',
+                dataType: "json",
+                crossDomain: true,
+                contentType: 'application/json; charset=utf-8',
+                success: function(data) {
+                    // console.log('success')
+                    var pageList = new Array();
+                    for (var i = 0; i < 3; i++) {
+                        pageList.push(data.data);
+                    }
+                    layout.$data = pageList[0];
+                },
+                error: function(err) {
+                    // console.log(err)
+                }
+            });
+        }
+    }
 });
 layout.new();
-
-
-// person.show();person.change();
-
-
-
-
--- js/index.js
+++ js/index.js
@@ -1,132 +1,290 @@
+//返回顶部
+var backTopObj = function() {
+    // alert("回到顶部");
+    this.demo;
+    this.bTop;
+    this.timer;
+    this.speed = 0;
+    this.isShowTop = false;
+    this.isHideTop = false;
+    this.hideTopTimer;
+}
+backTopObj.prototype.init = function() {
+    // alert("回到顶部");
+    if (document.documentElement.scrollTop >= 500 || document.body.scrollTop >= 500) {
+        this.isShowTop = true;
+    } else {
+        this.isHideTop = true;
+    }
+    this.demo = document.getElementById('back_top');
+    this.demo.addEventListener('click', back, false);
+    document.body.addEventListener('mousewheel', function() { clearInterval(backTop.timer); }, false);
+}
+backTopObj.prototype.showTop = function() {
+    this.isShowTop = false;
+    this.isHideTop = true;
+    clearInterval(this.hideTopTimer);
+    this.demo.style.display = 'block';
+    this.demo.style.opacity = 1;
+    this.demo.style.transition = 'opacity 0.7s';
+}
+backTopObj.prototype.hideTop = function() {
+    this.isShowTop = true;
+    this.isHideTop = false;
+    this.demo.style.opacity = 0;
+    this.demo.style.transition = 'opacity 0.7s';
+    this.hideTopTimer = setInterval(function() {
+        backTop.demo.style.display = 'none';
+        clearInterval(this.hideTopTimer);
+    }, 1000);
+}
+
+function back() {
+    clearInterval(backTop.timer);
+    backTop.timer = setInterval(function() {
+        backTop.bTop = document.documentElement.scrollTop || document.body.scrollTop;
+        backTop.speed = Math.floor(-backTop.bTop / 6);
+        document.documentElement.scrollTop = document.body.scrollTop = backTop.bTop + backTop.speed;
+        if (backTop.bTop <= 0) {
+            clearInterval(backTop.timer);
+        }
+    }, 30);
+}
+//返回顶部按钮和导航栏的显示和隐藏
+backTopObj.prototype.backTopBtn = function() {
+        // alert("111");
+        if (document.documentElement.scrollTop >= 500 || document.body.scrollTop >= 500) {
+            if (this.isShowTop) {
+                this.showTop();
+            }
+        } else if (document.documentElement.scrollTop <= 500 || document.body.scrollTop <= 500) {
+            if (this.isHideTop) {
+                this.hideTop();
+            }
+        } else {
+            return;
+        }
+    }
+//导航栏
+var navBarObj = function() {
+    this.left;
+    this.demo;
+    this.navs;
+    this.height = 0.000;
+}
+navBarObj.prototype.init = function() {
+    this.position = '';
+    this.left = document.documentElement.scrollLeft || document.body.scrollLeft;
+    this.demo = getDemo('nav_bar');
+    this.navs = getDemo('navs').getElementsByClassName('nav');
+    this.height = this.navs[0].offsetHeight;
+    for (var i = 1; i < this.navs.length - 1; i++) {
+        this.navs[i].getElementsByTagName('ul')[0].style.top = this.height + 'px';
+    }
+}
+navBarObj.prototype.check = function() {
+        if (document.documentElement.scrollTop >= 135 || document.body.scrollTop >= 135) {
+            this.demo.style.position = 'fixed';
+            this.demo.style.left = -this.left + 'px';
+        } else {
+            this.demo.style.position = '';
+        }
+    }
+    //搜索
+var searchObj = function() {
+    this.text;
+    this.searchInput;
+    this.searchBtn;
+}
+searchObj.prototype.init = function() {
+    this.text = '';
+    this.searchInput = getDemo('searchInput');
+    this.searchBtn = getDemo('search');
+    this.searchBtn.addEventListener('click', checkSearch, false);
+}
+
+function checkSearch() {
+    search.text = search.searchInput.value;
+    if (len(search.text) > 2) {
+        window.open('http://home.ncu.edu.cn/NewIndex2013/Article_search.aspx?KeyWord=' + encodeURI(search.text));
+        searchInput.value = '';
+    } else if (len(search.text) == 0) {
+        return;
+    } else {
+        alert('搜索的内容太短了');
+    }
+}
+
+function len(s) {
+    var l = 0;
+    var a = s.split('');
+    for (var i = 0; i < a.length; i++) {
+        if (a[i].charCodeAt(0) < 299) {
+            l++;
+        } else {
+            l += 2;
+        }
+    }
+    return l;
+}
+//底部二维码
 var before = ['img/share_qq.png', 'img/share_weibo.png', 'img/share_weixin.png'];
 var after = ['img/share_qq_after.png', 'img/share_weibo_after.png', 'img/share_weixin_after.png'];
 var erSrc = ['img/QQ_code.jpg', 'img/weibo_code.jpg', 'img/weixiner.png'];
 
 function changeer() {
-	var erContainer = document.getElementById('erContainer');
-	var sss = erContainer.getElementsByTagName('a');
-	for (var i = 0; i < sss.length; i++) {
-		sss[i].onmouseenter = function() {
-			var thisIndex = this.getAttribute('index');
-			for (var j = 0; j < sss.length; j++) {
-				sss[j].style.backgroundImage = 'url(' + before[j] + ')';
-			}
-			this.style.backgroundImage = 'url(' + after[thisIndex - 1] + ')';
-			erContainer.getElementsByTagName('img')[0].src = erSrc[thisIndex - 1];
-		}
-	}
+    var erContainer = document.getElementById('erContainer');
+    var sss = erContainer.getElementsByTagName('a');
+    for (var i = 0; i < sss.length; i++) {
+        sss[i].onmouseenter = function() {
+            var thisIndex = this.getAttribute('index');
+            for (var j = 0; j < sss.length; j++) {
+                sss[j].style.backgroundImage = 'url(' + before[j] + ')';
+            }
+            this.style.backgroundImage = 'url(' + after[thisIndex - 1] + ')';
+            erContainer.getElementsByTagName('img')[0].src = erSrc[thisIndex - 1];
+        }
+    }
 }
 changeer();
-//固定右边下载和人物在页面上的高度
-function fixHight() {
-	var right=document.querySelector(".right-content");
-	
-	console.log(document.documentElement.clientHeight);
-	console.log(document.documentElement.scrollTop);
-	console.log(document.body.scrollTop);
-	console.log(document.body.scrollTop);
-	if (document.documentElement.scrollTop>0||document.body.scrollTop>0||document.body.scrollTop>0) {
-		alert("2333");
-	}
-	// var clientHeight = document.documentElement.clientHeight;
-	// var top = document.documentElement.scrollTop || document.body.scrollTop;
-
-}
-fixHight();
 
 (function($) {
-	$("div[data-scro='controler'] b,div[data-scro='controler2'] a").click(function() {
-		var T = $(this);
-		if (T.attr("class") == "down") return false; //如果class属性值有down
-		else j2rolling_animation.st({
-			findObject: T, //var T=$(this); 这是选定当前对象
-			main: T.parent().parent().find("div[data-scro='list']"), //滚动目标容器窗口对象
-			pagSource: T.parent().parent().find("div[data-scro='controler'] b"), //切换按钮对象
-			className: "down", //选中的样式
-			duration: "slow", //滚动速度 和jquery速度一致
-			on: $(this)[0].tagName == "A" ? true : false //用于判断是否开启无限滚动 or 来回切换
-		});
-		return false;
-	});
+    $("div[data-scro='controler'] b,div[data-scro='controler2'] a").click(function() {
+        var T = $(this);
+        if (T.attr("class") == "down") return false; //如果class属性值有down
+        else j2rolling_animation.st({
+            findObject: T, //var T=$(this); 这是选定当前对象
+            main: T.parent().parent().find("div[data-scro='list']"), //滚动目标容器窗口对象
+            pagSource: T.parent().parent().find("div[data-scro='controler'] b"), //切换按钮对象
+            className: "down", //选中的样式
+            duration: "slow", //滚动速度 和jquery速度一致
+            on: $(this)[0].tagName == "A" ? true : false //用于判断是否开启无限滚动 or 来回切换
+        });
+        return false;
+    });
 
-	var j2setTime = "",
-		j2time = true,
-		j2rolling_animation = {
-			init: function() {
-				this.start();
-				this.time();
-			},
-			st: function(o) {
-				if (j2time) {
-					this.animate(o.findObject, o.main, o.className, o.duration, o.pagSource, o.on);
-					j2time = false;
-				}
-			},
-			animate: function(T, M, C, S, P, O) {
-				//find()方法获得当前元素集合中每个元素的后代，通过选择器、jQuery 对象或元素来筛选。
-				var _prevDown = O ? P.parent().find("*[class='" + C + "']") : T.parent().find(T[0].tagName + "[class='" + C + "']"),
-					_prevIndex = _prevDown.index(),
-					_thisIndex = O ? (T.attr("class") == "next" ? _prevIndex + 1 : _prevIndex - 1) : T.index(),
-					_list = M.find(".item"),
-					p2n = 1;
-				_prevDown.removeClass(C);
-				if (O) {
-					if (_thisIndex == -1) _thisIndex = _list.size() - 1;
-					if (_thisIndex == _list.size()) _thisIndex = 0;
-					P.eq(_thisIndex).addClass(C);
-				} else {
-					T.addClass(C);
-				};
-				if (T.attr("class") == "prev" || _thisIndex < _prevIndex) p2n = false;
-				if ((T.attr("class") == "next" || _thisIndex > _prevIndex) && (T.attr("class") != "prev")) p2n = true;
+    var j2setTime = "",
+        j2time = true,
+        j2rolling_animation = {
+            init: function() {
+                this.start();
+                this.time();
+            },
+            st: function(o) {
+                if (j2time) {
+                    this.animate(o.findObject, o.main, o.className, o.duration, o.pagSource, o.on);
+                    j2time = false;
+                }
+            },
+            animate: function(T, M, C, S, P, O) {
+                //find()方法获得当前元素集合中每个元素的后代，通过选择器、jQuery 对象或元素来筛选。
+                var _prevDown = O ? P.parent().find("*[class='" + C + "']") : T.parent().find(T[0].tagName + "[class='" + C + "']"),
+                    _prevIndex = _prevDown.index(),
+                    _thisIndex = O ? (T.attr("class") == "next" ? _prevIndex + 1 : _prevIndex - 1) : T.index(),
+                    _list = M.find(".item"),
+                    p2n = 1;
+                _prevDown.removeClass(C);
+                if (O) {
+                    if (_thisIndex == -1) _thisIndex = _list.size() - 1;
+                    if (_thisIndex == _list.size()) _thisIndex = 0;
+                    P.eq(_thisIndex).addClass(C);
+                } else {
+                    T.addClass(C);
+                };
+                if (T.attr("class") == "prev" || _thisIndex < _prevIndex) p2n = false;
+                if ((T.attr("class") == "next" || _thisIndex > _prevIndex) && (T.attr("class") != "prev")) p2n = true;
 
-				!p2n ? _list.eq(_thisIndex).css("left", -M.width()) : '';
-				_list.eq(_prevIndex).animate({
-					left: p2n ? -M.width() : M.width()
-				}, S, function() {
-					$(this).removeAttr("style");
-					j2time = true;
-				});
-				_list.eq(_thisIndex).animate({
-					left: "0px"
-				}, S);
-			},
-			start: function() {
-				$("#section-focus-pic div[data-scro='controler'] b,#section-focus-pic div[data-scro='controler2'] a").mouseover(function() {
-					window.clearInterval(j2setTime);
-				}).mouseout(function() {
-					j2rolling_animation.time();
-				});
-			},
-			time: function() {
-				j2setTime = window.setInterval(function() {
-					var num = $("#section-focus-pic div[data-scro='controler'] b[class='down']").index();
-					var _list = $("#section-focus-pic div[data-scro='list'] li");
+                !p2n ? _list.eq(_thisIndex).css("left", -M.width()) : '';
+                _list.eq(_prevIndex).animate({
+                    left: p2n ? -M.width() : M.width()
+                }, S, function() {
+                    $(this).removeAttr("style");
+                    j2time = true;
+                });
+                _list.eq(_thisIndex).animate({
+                    left: "0px"
+                }, S);
+            },
+            start: function() {
+                $("#section-focus-pic div[data-scro='controler'] b,#section-focus-pic div[data-scro='controler2'] a").mouseover(function() {
+                    window.clearInterval(j2setTime);
+                }).mouseout(function() {
+                    j2rolling_animation.time();
+                });
+            },
+            time: function() {
+                j2setTime = window.setInterval(function() {
+                    var num = $("#section-focus-pic div[data-scro='controler'] b[class='down']").index();
+                    var _list = $("#section-focus-pic div[data-scro='list'] li");
 
-					// _list.eq(num).animate({"left":$("#section-focus-pic div[data-scro='list']").width()},"slow",function(){
-					//eq(1) 选取带有指定index的值
-					//$(selector).animate(styles,speed,easing,callback)
-					_list.eq(num).animate({
-						"left": -$("#section-focus-pic div[data-scro='list']").width()
-					}, "slow", function() {
-						$(this).removeAttr("style");
-						$("#section-focus-pic div[data-scro='controler'] b").removeClass("down").eq(num).addClass("down");
-					});
-					num++;
-					//size()输出被jQuery选择器匹配的元素的数量
-					if (num == _list.size()) {
-						num = 0;
-					}
-					_list.eq(num).animate({
-						"left": "0px"
-					}, "slow");
-				}, 4000);
-			}
-		};
-	$("a").click(function() {
-		$(this).blur(); //blur()是失去焦点			  
-	});
+                    // _list.eq(num).animate({"left":$("#section-focus-pic div[data-scro='list']").width()},"slow",function(){
+                    //eq(1) 选取带有指定index的值
+                    //$(selector).animate(styles,speed,easing,callback)
+                    _list.eq(num).animate({
+                        "left": -$("#section-focus-pic div[data-scro='list']").width()
+                    }, "slow", function() {
+                        $(this).removeAttr("style");
+                        $("#section-focus-pic div[data-scro='controler'] b").removeClass("down").eq(num).addClass("down");
+                    });
+                    num++;
+                    //size()输出被jQuery选择器匹配的元素的数量
+                    if (num == _list.size()) {
+                        num = 0;
+                    }
+                    _list.eq(num).animate({
+                        "left": "0px"
+                    }, "slow");
+                }, 4000);
+            }
+        };
+    $("a").click(function() {
+        $(this).blur(); //blur()是失去焦点             
+    });
 
-	j2rolling_animation.init(); //是否开启自动轮播
+    j2rolling_animation.init(); //是否开启自动轮播
 })(this.jQuery);
+
+//开启功能
+var backTop;
+var navBar;
+
+var er;
+var search;
+
+function getDemo(ID) {
+    return document.getElementById(ID);
+}
+window.onload = function() {
+	// alert("1112");
+    document.getElementsByTagName('html')[0].style.fontSize = (62.5 / 1366) * window.screen.availWidth + "%";
+
+    backTop = new backTopObj();
+    backTop.init();
+    backTop.backTopBtn();
+
+    navBar = new navBarObj();
+    navBar.init();
+    navBar.check();
+
+    search = new searchObj();
+    search.init();
+
+
+    // er = new erObj();
+    // er.init();
+    // for (var i = 0; i < er.num; i++) {
+    //     er.changeBtn[i].addEventListener('mouseover', changeEr, false);
+    // }
+
+
+
+    window.onscroll = function() {
+        navBar.left = document.documentElement.scrollLeft || document.body.scrollLeft;
+        backTop.backTopBtn();
+        navBar.check();
+    }
+}
+
+
+
 
